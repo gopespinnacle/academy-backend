3e4ee1de2eea106c3599a53211ca40774b211bca
Vishnu <gopestalks@gmail.com>
2026-04-17 18:52:09 +0530

    fix cors

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ const app = express();
 /* ================= MIDDLEWARE ================= */
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
 
     if (req.method === "OPTIONS") {
         return res.sendStatus(200);
