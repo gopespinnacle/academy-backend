71c6ecd816a156d42c7c120aa38436e19f8c1496
gopespinnacle <gopestalks@gmail.com>
2026-04-23 12:29:52 +0530

    password reset

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,4 +9,29 @@ router.post("/register", registerUser);
 /* LOGIN */
 router.post("/login", loginUser);
 
+
+router.post("/reset-password", async (req,res)=>{
+try{
+
+const { email, oldPassword, newPassword } = req.body;
+
+const user = await User.findOne({ loginEmail: email });
+
+if(!user){
+return res.json({ message:"User not found ❌" });
+}
+
+if(user.loginPassword !== oldPassword){
+return res.json({ message:"Old password wrong ❌" });
+}
+
+user.loginPassword = newPassword;
+await user.save();
+
+res.json({ message:"Password updated successfully ✅" });
+
+}catch(err){
+res.status(500).json({ message:"Error" });
+}
+});
 module.exports = router;
