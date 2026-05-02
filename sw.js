3bc902f600db4f3167dedaf1668c3e0b1e02884b
gopespinnacle <gopestalks@gmail.com>
2026-05-02 13:57:38 +0530

    asd

--- sw.js
+++ sw.js
@@ -1,7 +1,27 @@
+const CACHE_NAME = "academy-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/icon.png",
+  "/icon-512.png"
+];
+
+// INSTALL
 self.addEventListener("install", (event) => {
-    console.log("SW Installed");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
+// FETCH (LOAD FROM CACHE)
 self.addEventListener("fetch", (event) => {
-    // basic pass-through
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
