2d1c04374912ed9352c1e8639e8c188734ccdd4f
gopespinnacle <gopestalks@gmail.com>
2026-05-02 14:05:42 +0530

    dsf

--- sw.js
+++ sw.js
@@ -1,26 +1,24 @@
-const CACHE_NAME = "academy-v1";
+const CACHE_NAME = "gopes-cache-v1";
 
 const urlsToCache = [
   "/",
   "/index.html",
-  "/style.css",
-  "/icon.png",
-  "/icon-512.png"
+  "/style.css"
 ];
 
 // INSTALL
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
+    caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(urlsToCache);
     })
   );
 });
 
-// FETCH (LOAD FROM CACHE)
-self.addEventListener("fetch", (event) => {
+// FETCH
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
+    caches.match(event.request).then(response => {
       return response || fetch(event.request);
     })
   );
