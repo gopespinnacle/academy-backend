b330ad1b0bd5732e050d9ababb88c02816a95835
gopespinnacle <gopestalks@gmail.com>
2026-05-10 13:59:44 +0530

    Fixed uploads path

--- routes/founderRoutes.js
+++ routes/founderRoutes.js
@@ -499,7 +499,7 @@ cloudinary.config({
 const storage = multer.diskStorage({
 
     destination: function (req, file, cb) {
-        cb(null, "uploads/");
+        cb(null, path.join(__dirname, "../uploads"));
     },
 
     filename: function (req, file, cb) {
