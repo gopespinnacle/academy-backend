42246a7963a86ed6b0462462efa5b5fcf679c6a0
gopespinnacle <gopestalks@gmail.com>
2026-05-12 14:57:44 +0530

    Fix Google Drive upload

--- googleDriveUpload.js
+++ googleDriveUpload.js
@@ -1,56 +1,53 @@
 const fs = require("fs");
 const { google } = require("googleapis");
 
-const SCOPES = [
-    "https://www.googleapis.com/auth/drive"
-];
-
 const credentials = JSON.parse(
     process.env.GOOGLE_CREDENTIALS
 );
 
 const auth = new google.auth.GoogleAuth({
     credentials,
-    scopes: SCOPES,
+    scopes: [
+        "https://www.googleapis.com/auth/drive"
+    ]
 });
 
 const drive = google.drive({
     version: "v3",
-    auth,
+    auth
 });
 
 async function uploadFile(file){
 
-    const fileMetadata = {
-        name: file.originalname,
-
-        parents: [
-    "1O6Zn8xlD_PCvtZOzG66OSqSSuTHOAp3k"
-]
-    };
-
-    const media = {
-        mimeType: file.mimetype,
-        body: fs.createReadStream(file.path),
-    };
-
     const response = await drive.files.create({
-        resource: fileMetadata,
-        media,
-        fields: "id",
+
+        requestBody: {
+            name: file.originalname
+        },
+
+        media: {
+            mimeType: file.mimetype,
+            body: fs.createReadStream(file.path)
+        },
+
+        fields: "id"
     });
 
     await drive.permissions.create({
+
         fileId: response.data.id,
+
         requestBody: {
             role: "reader",
-            type: "anyone",
-        },
+            type: "anyone"
+        }
+
     });
 
     fs.unlinkSync(file.path);
 
     return `https://drive.google.com/file/d/${response.data.id}/view`;
+
 }
 
 module.exports = uploadFile;
