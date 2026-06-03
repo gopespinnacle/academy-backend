7bd766b660eef8d574d3a363db83128921edf515
gopespinnacle <gopestalks@gmail.com>
2026-06-03 13:50:34 +0530

    xvxvxcvxv

--- routes/homeworkRoutes.js
+++ routes/homeworkRoutes.js
@@ -52,32 +52,23 @@ console.log(
 credentials.private_key.length
 );
 
-const auth = new google.auth.JWT(
+const auth = new google.auth.JWT({
+  email: credentials.client_email,
+  key: credentials.private_key,
+  scopes: [
+    "https://www.googleapis.com/auth/drive"
+  ]
+});
 
-credentials.client_email,
-
-null,
-
-credentials.private_key,
-
-[
-"https://www.googleapis.com/auth/drive"
-]
-
-);
+const drive = google.drive({
+  version: "v3",
+  auth: auth
+});
 
 console.log(
 "JWT EMAIL:",
 credentials.client_email
 );
-
-const drive = google.drive({
-
-version:"v3",
-
-auth: auth
-
-});
 
 router.post(
 "/upload-homework",
