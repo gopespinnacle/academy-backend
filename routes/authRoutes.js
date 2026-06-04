e19ac634b169d0feb17e6a10f47f3f009f726227
gopespinnacle <gopestalks@gmail.com>
2026-06-04 11:21:11 +0530

    sfsd

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,5 +8,30 @@ router.post("/register", registerUser);
 /* LOGIN */
 router.post("/login", loginUser);
 
+router.get(
+"/students",
+async(req,res)=>{
+
+try{
+
+const students =
+await User.find({
+role:"student"
+});
+
+res.json(
+students
+);
+
+}catch(err){
+
+res.status(500).json({
+message:"Server Error"
+});
+
+}
+
+});
+
 
 module.exports = router;
