be6f75d39a03799d51b95ccc09934fff7809b8b3
gopespinnacle <gopestalks@gmail.com>
2026-06-25 15:23:00 +0530

    sdfdsf

--- config/s3.js
+++ config/s3.js
@@ -12,8 +12,8 @@ const {
 const client = new S3Client({
     region: process.env.AWS_REGION,
     credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY
 }
 });
 
