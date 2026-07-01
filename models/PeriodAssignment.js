ef851a9a2a029166fc24aab4ffbad477f40df598
gopespinnacle <gopestalks@gmail.com>
2026-07-01 12:01:48 +0530

    asdsad

--- models/PeriodAssignment.js
+++ models/PeriodAssignment.js
@@ -2,29 +2,29 @@ const mongoose = require("mongoose");
 
 const periodAssignmentSchema = new mongoose.Schema({
 
-teacher: {
-type: mongoose.Schema.Types.ObjectId,
-ref: "User"
-},
+    teacher:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
 
-students: [
-    {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
-    }
-],
+    className:String,
+    subject:String,
+    day:String,
+    startTime:String,
+    endTime:String,
 
-className: String,
-subject: String,
+    assignments:[
+        {
+            student:{
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"User"
+            },
+            subjects:[String],
+            languages:[String],
+            eca:[String]
+        }
+    ]
 
-day: String,
-startTime: String,
-endTime: String,
-
-subjects: [String],
-languages: [String],
-eca: [String]
-
-});
+},{timestamps:true});
 
 module.exports = mongoose.model("PeriodAssignment", periodAssignmentSchema);
