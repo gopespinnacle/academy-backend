1722cb0187eb4e7d760bec405b55cb53e6c45259
gopespinnacle <gopestalks@gmail.com>
2026-07-20 21:41:00 +0530

    sdfsdfdsf

--- routes/aiAssessmentRoutes.js
+++ routes/aiAssessmentRoutes.js
@@ -8,7 +8,7 @@ const upload = multer({
 });
 
 const aiAssessmentController =
-    require("../controllers/aiAssessmentController");
+require("../controllers/AIAssessmentController");
 
 /*
 ====================================================
